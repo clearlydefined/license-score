b6f7ad16c51a2fb25320093e33152ff2481b0a72
Philippe Ombredanne <pombredanne@nexb.com>
2018-10-01 16:42:24 +0200

    Add scoring computation example script and data
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ scoring/README.md
@@ -0,0 +1,22 @@
+# clearlylicensed-test Utility
+
+This Python utility will take the `download_url` values from `clearlylicensed.csv`, and will
+download them, extract them, then scan them with scancode using specific options to compute a score. 
+
+The `license_score` data from the scan results will then be added to the correct row in 
+the output csv file: `clearlylicensed-out.csv`. 
+These values are hardcoded for now. 
+
+The scans are kept in the `scans` dir and the downloaded packages are in `packages`.
+
+
+** NOTE **: You will need to be in a scancode-toolkit virtualenv for this script
+to work or to have scancode-toolkit installed and install the `requirements.txt` with pip.
+
+To recreate the `clearlylicensed-out.csv` score collection:
+
+- `git clone https://github.com/clearlydefined/license-score`
+- `git clone https://github.com/nexB/scancode-toolkit` and `cd scancode-toolkit`
+- run `./configure` then `source bin/activate`
+- `cd ../license-score/scoring`
+- `python clearlylicensed-test.py`
---
+++ scoring/clearlylicensed-test.py
@@ -0,0 +1,153 @@
+
+from __future__ import print_function
+
+from collections import OrderedDict
+import csv
+import json
+import os
+import glob
+from subprocess import call
+
+import requests
+
+
+current_dir = os.path.dirname(__file__)
+
+
+def create_dir(name, base_dir=current_dir):
+    d = os.path.join(base_dir, name)
+    try:
+        os.makedirs(d)
+    except OSError:
+        if not os.path.isdir(d):
+            raise
+    return d
+
+
+def fetch_package(url, downloads_dir):
+    """
+    Download the package at `url` in `download_dir` and return its filename.
+    """
+    import cgi
+    import socket
+    import requests
+    from requests.exceptions import RequestException
+
+    MAX_RETRIES = 3
+    success = False
+
+    for i in range(MAX_RETRIES):
+        try:
+            response = requests.get(url, timeout=20)
+            print('response.status_code:', response.status_code,)
+            if response.status_code == 200:
+                success = True
+                break
+            else:
+                import time
+                time.sleep(1)
+        except (RequestException, socket.timeout) as e:
+            raise e
+
+    if not success:
+        raise Exception('Could not download content ({MAX_RETRIES} retries).'.format(**locals()))
+
+    content_type = response.headers.get('content-type', '').lower()
+
+    content_disposition = response.headers.get('content-disposition', '')
+    value, params = cgi.parse_header(content_disposition)
+    filename = params.get('filename') or url.split('/')[-1]
+
+    with open(os.path.join(downloads_dir, filename), 'wb') as out:
+        out.write(response.content)
+    return filename
+
+
+def extract(archive_loc):
+    call(' '.join([
+        'extractcode',
+        '--shallow',
+        str(archive_loc)
+        ]),
+        shell=True
+    )
+
+
+def scan(extracted_archive_loc, json_scan_loc, csv_scan_loc):
+    call(' '.join([
+        'scancode',
+        '--copyright',
+        '--license',
+        '--license-text',
+        '--license-diag',
+        '--info',
+        '--classify',
+        '--license-clarity-score',
+        '--summary',
+        '--summary-key-files',
+         '-n', '4',
+        '--json-pp', json_scan_loc,
+        '--csv', csv_scan_loc,
+         extracted_archive_loc
+         ]),
+        shell=True
+    )
+
+
+def get_packages(csv_loc='clearlylicensed.csv'):
+    """
+    Read the CSV at csv_loc and yield ordered mapping from package data
+    """
+    with open(csv_loc, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        empty_template_row = OrderedDict((f, None) for f in reader.fieldnames)
+
+        for row in reader:
+            item = OrderedDict(empty_template_row)
+            item.update(row)
+            yield item
+
+
+def process(input_csv='clearlylicensed.csv', output_csv='clearlylicensed-out.csv'):
+    downloads_dir = create_dir('packages')
+    scan_results_dir = create_dir('scans')
+
+    # hardcode headers to keep order
+    headers = 'download_url type namespace name version qualifier'.split()
+
+    # list of mappings
+    results = []
+
+    for package in get_packages(csv_loc=input_csv):
+        download_url = package.get('download_url')
+        print('===========================================================')
+        print('* Processing:', download_url)
+        filename = fetch_package(download_url, downloads_dir)
+
+        archive_loc = os.path.join(downloads_dir, filename)
+        json_scan_loc = os.path.join(scan_results_dir, filename + '-clarity.json')
+        csv_scan_loc = os.path.join(scan_results_dir, filename + '-clarity.csv')
+        extracted_archive_loc = archive_loc + '-extract'
+
+        extract(archive_loc)
+        scan(extracted_archive_loc, json_scan_loc, csv_scan_loc)
+
+        scan_result = json.loads(
+            open(json_scan_loc).read(),
+            object_pairs_hook=OrderedDict
+        )
+
+        license_score = scan_result.get('license_score')
+        package.update(license_score)
+        package['scan_results_file'] = json_scan_loc
+        results.append(package)
+
+    headers = results[0].keys()
+    with open(output_csv, 'wb') as outfile:
+        dict_writer = csv.DictWriter(outfile, headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(results)
+
+
+if __name__ == '__main__':
+    process()
