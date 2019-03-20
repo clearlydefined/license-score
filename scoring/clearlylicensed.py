b44a8c69c90da20ab74f05320c5615b86636c706
Philippe Ombredanne <pombredanne@nexb.com>
2019-03-20 19:34:26 +0100

    Update script to fetch optionally
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- scoring/clearlylicensed.py
+++ scoring/clearlylicensed.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 import csv
 import json
 import os
-from subprocess import call
+from subprocess import check_call, call
 import sys
 import traceback
 
@@ -21,6 +21,7 @@ except ImportError:
 import requests
 
 
+current_dir = os.path.abspath(os.path.dirname(__file__))
 
 """
 Process a list of popular software packages to compute a license clarity score.
@@ -32,9 +33,12 @@ This script performs the following steps:
 4. collate all scans results in a CSV.
 """
 
-def compute_license_score(input_csv='clearlylicensed.csv',
-        output_csv='clearlylicensed-out.csv',
-        fetch=True, scan=True):
+
+def compute_license_score(input_csv='5000-packages-license-score-data.csv',
+        output_csv='5000-packages-license-score-results-out.csv',
+        base_dir=current_dir,
+        do_fetch=True,
+        do_extract=True):
     """
     Read the package data in the CSV at `input_csv` and write a CSV at
     `output_csv`. The `input_csv` must contain these columns: download_url and
@@ -46,45 +50,54 @@ def compute_license_score(input_csv='clearlylicensed.csv',
     in the "scans" directory.
 
     """
-    download_dir = create_dir('packages')
-    scan_results_dir = create_dir('scans')
+    downloads_dir = create_dir('downloads', base_dir=base_dir)
+    extracts_dir = create_dir('extracts', base_dir=base_dir)
+    scans_dir = create_dir('scans', base_dir=base_dir)
 
     # list of mappings
     results = []
 
-    for package in get_packages_data(csv_loc=input_csv):
+    for i, package in enumerate(get_packages_data(csv_loc=input_csv)):
         download_url = package.get('download_url')
         pkg_type = package.get('type')
         pkg_name = package.get('name')
         print('===========================================================')
-        print('* Processing:', download_url)
+        print(i, 'Processing: ', download_url)
 
-        fetch_file = fetch
-        # filename MUST be present in CSV, otherwise the packages are fetched
-        filename = package.get('filename')
-        if filename:
-            if not fetch and not os.path.exists(os.path.join(download_dir, filename)):
+        archive_filename = package.get('filename')
+        downloaded_archive_loc = os.path.join(downloads_dir, pkg_type, archive_filename)
+        archive_extract_dir = archive_filename + '-extract'
+        extracted_archive_loc = os.path.join(downloads_dir, pkg_type, archive_extract_dir)
+        target_extracted_archive_loc = os.path.join(extracts_dir, pkg_type, archive_extract_dir)
+
+        if do_fetch:
+            fetch_file = fetch
+            # filename MUST be present in CSV, otherwise the packages are fetched
+            if archive_filename:
+                if not os.path.exists(downloaded_archive_loc):
+                    fetch_file = True
+            else:
                 fetch_file = True
-        else:
-            fetch_file = True
 
-        if fetch_file:
-            try:
-                filename = fetch_package(download_url, pkg_type, pkg_name, download_dir)
-            except Exception:
-                package['scan_results_file'] = 'FAILED DOWNLOAD URL:\n{}'.traceback.format_exc()
-                results.append(package)
-                continue
+            if fetch_file:
+                try:
+                    archive_filename = fetch_package(download_url, pkg_type, pkg_name, downloads_dir)
+                except Exception:
+                    package['scan_results_file'] = 'FAILED DOWNLOAD URL:\n{}'.traceback.format_exc()
+                    results.append(package)
+                    continue
 
-        archive_loc = os.path.join(download_dir, filename)
-        extracted_archive_loc = archive_loc + '-extract'
+        if do_extract:
+            extract(downloaded_archive_loc)
 
-        extract(archive_loc)
+        # move the extracts to an extract dir
+        if os.path.exists(extracted_archive_loc):
+            os.rename(extracted_archive_loc, target_extracted_archive_loc)
 
         # TODO: fix key files location
         if type == 'gem':
             # we need to move files to root locations. Either we delete some (checksums, and archives after extraction...
-            # remove checksums?            
+            # remove checksums?
             pass
 
         if type == 'maven':
@@ -94,17 +107,17 @@ def compute_license_score(input_csv='clearlylicensed.csv',
             # META-INF/maven/<dotted-gid>/<aid>/pom.properties
             pass
 
-        json_scan_loc = os.path.join(scan_results_dir, filename + '-clarity.json')
-        csv_scan_loc = os.path.join(scan_results_dir, filename + '-clarity.csv')
+        json_scan_loc = os.path.join(scans_dir, pkg_type, archive_filename + '-clarity.json')
+        csv_scan_loc = os.path.join(scans_dir, pkg_type, archive_filename + '-clarity.csv')
 
-
-        scan(extracted_archive_loc, json_scan_loc, csv_scan_loc)
+        scan(target_extracted_archive_loc, json_scan_loc, csv_scan_loc)
 
         with open(json_scan_loc, 'rb') as scanned:
             scan_result = json.load(scanned, object_pairs_hook=OrderedDict)
 
-        license_score = scan_result.get('license_score')
+        license_score = scan_result.get('license_clarity_score')
         package.update(license_score)
+
         package['scan_results_file'] = json_scan_loc
         results.append(package)
 
@@ -169,10 +182,12 @@ def scan(extracted_archive_loc, json_scan_loc, csv_scan_loc):
         '--license-diag',
         '--info',
         '--classify',
+        '--timeout', '10',
+        '--max-in-memory', '0',
         '--license-clarity-score',
         '--summary',
         '--summary-key-files',
-         '-n', '4',
+         '-n', '3',
         '--json-pp', json_scan_loc,
         '--csv', csv_scan_loc,
          extracted_archive_loc
@@ -195,7 +210,7 @@ def get_packages_data(csv_loc='clearlylicensed.csv'):
             yield item
 
 
-def create_dir(name, base_dir=os.path.dirname(__file__)):
+def create_dir(name, base_dir=current_dir):
     d = os.path.join(base_dir, name)
     try:
         os.makedirs(d)
@@ -212,6 +227,10 @@ if __name__ == '__main__':
     if args:
         arg0 = args[0]
         if arg0 == '--no-fetch':
-            fetch = False
+            do_fetch = False
+            do_extract = False
 
-    compute_license_score(fetch=fetch)
+    base_dir = os.environ.get('CLEARDATA', current_dir)
+
+    compute_license_score(
+        do_fetch=do_fetch, do_extract=do_extract, base_dir=base_dir)
