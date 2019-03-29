c79012b13e3bd141eb15900c2d997e0c6b6d9f72
Philippe Ombredanne <pombredanne@nexb.com>
2019-03-29 13:49:41 +0100

    Generate aggregated tables
    This is a better way than to compute these in a spreadsheet
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- scoring/clearlylicensed.py
+++ scoring/clearlylicensed.py
@@ -2,8 +2,11 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+from __future__ import division
+
 
 import cgi
+from collections import namedtuple
 from collections import OrderedDict
 import csv
 import json
@@ -11,13 +14,14 @@ import os
 from subprocess import call
 import sys
 import traceback
+from collections import Counter
 
 try:
     from urllib.parse import urlparse
 except ImportError:
     from urlparse import urlparse
 
-
+import attr
 import requests
 
 
@@ -34,8 +38,26 @@ This script performs the following steps:
 """
 
 
-def compute_license_score(input_csv='5000-packages-license-score-data.csv',
+brackets_labels = OrderedDict([
+    (0, '0'),
+    (1, '1 to 9'),
+    (10, '10 to 19'),
+    (20, '20 to 29'),
+    (30, '30 to 39'),
+    (40, '40 to 49'),
+    (50, '50 to 59'),
+    (60, '60 to 69'),
+    (70, '70 to 79'),
+    (80, '80 to 89'),
+    (90, '90 to 99'),
+    (100, '100'),
+])
+
+
+def compute_license_score(
+        input_csv='5000-packages-license-score-data.csv',
         output_csv='5000-packages-license-score-results-out.csv',
+        aggregates_csv='5000-packages-license-score-results-aggregates.csv',
         base_dir=current_dir,
         do_fetch=True,
         do_extract=True,
@@ -60,7 +82,15 @@ def compute_license_score(input_csv='5000-packages-license-score-data.csv',
     # list of mappings
     results = []
 
-    for i, package in enumerate(get_packages_data(csv_loc=input_csv)):
+    package_data = get_packages_data(csv_loc=input_csv)
+
+    # uncomment for testing on a smaller subset
+    #     package_data = list(package_data)
+    #     import random
+    #     random.shuffle(package_data)
+    #     package_data = package_data[:30]
+
+    for i, package in enumerate(package_data):
         pkg_type = package.get('type')
         if types and pkg_type not in types:
             continue
@@ -103,19 +133,6 @@ def compute_license_score(input_csv='5000-packages-license-score-data.csv',
         if os.path.exists(extracted_archive_loc):
             os.rename(extracted_archive_loc, target_extracted_archive_loc)
 
-        # TODO: fix key files location
-        if type == 'gem':
-            # we need to move files to root locations. Either we delete some (checksums, and archives after extraction...
-            # remove checksums?
-            pass
-
-        if type == 'maven':
-            # get META-INF/maven bits
-            # the shape is:
-            # META-INF/maven/<dotted-gid>/<aid>/pom.xml
-            # META-INF/maven/<dotted-gid>/<aid>/pom.properties
-            pass
-
         json_scan_loc = os.path.join(scans_dir, pkg_type, archive_filename + '-clarity.json')
         csv_scan_loc = os.path.join(scans_dir, pkg_type, archive_filename + '-clarity.csv')
 
@@ -145,8 +162,32 @@ def compute_license_score(input_csv='5000-packages-license-score-data.csv',
             renamed = 'score', 'declared', 'discovered', 'consistency', 'spdx', 'full_text'
             kvs = zip(renamed, license_score.values())
             license_score = OrderedDict(kvs)
-            license_score['score_bracket'] = int(round(int(license_score['score']) / 10, 1)) * 10
-            license_score['discovered_bracket'] = int(round(float(license_score['discovered']) * 10, 1)) * 10
+
+            # convert booleans to 0/1
+            decl = license_score['declared']
+            license_score['declared'] = 1 if decl else 0
+            cons = license_score['consistency']
+            license_score['consistency'] = 1 if cons else 0
+            spdx = license_score['spdx']
+            license_score['spdx'] = 1 if spdx else 0
+            full_text = license_score['full_text']
+            license_score['full_text'] = 1 if full_text else 0
+
+            # compute brackets
+            score = int(license_score['score'])
+            score_bracket = int(round(score / 10, 1)) * 10
+            if score_bracket == 0:
+                if score != 0:
+                    score_bracket = 1
+            license_score['score_bracket'] = score_bracket
+
+            # compute brackets
+            disco = float(license_score['discovered'])
+            discovered_bracket = int(round(disco * 10, 1)) * 10
+            if discovered_bracket == 0:
+                if disco != 0:
+                    discovered_bracket = 1
+            license_score['discovered_bracket'] = discovered_bracket
 
             package.update(license_score)
 
@@ -157,6 +198,219 @@ def compute_license_score(input_csv='5000-packages-license-score-data.csv',
         dict_writer = csv.DictWriter(outfile, headers)
         dict_writer.writeheader()
         dict_writer.writerows(results)
+
+    aggregate_tables = compute_aggregates(results)
+
+    with open(aggregates_csv, 'wb') as aggfile:
+        for name, aggregate_table in aggregate_tables.items():
+            aggfile.write(name + '\n')
+            aggregated = [
+                attr.asdict(aggregate, dict_factory=OrderedDict)
+                for aggregate in aggregate_table]
+
+            headers = aggregated[0].keys()
+            dict_writer = csv.DictWriter(aggfile, headers)
+            dict_writer.writeheader()
+            dict_writer.writerows(aggregated)
+            aggfile.write('\n\n')
+
+
+_dp_fields = [
+    'type',
+    'score', 'declared', 'discovered', 'consistency', 'spdx', 'full_text',
+    'score_bracket', 'discovered_bracket']
+
+dp_fields = set(_dp_fields)
+Datapoint = namedtuple('Datapoint', _dp_fields)
+
+
+@attr.s(slots=True)
+class Aggregate(object):
+    label = attr.ib()
+    gem = attr.ib(default=0)
+    maven = attr.ib(default=0)
+    npm = attr.ib(default=0)
+    nuget = attr.ib(default=0)
+    pypi = attr.ib(default=0)
+    total = attr.ib(default=0)
+
+    ptypes = 'gem', 'maven', 'npm', 'nuget', 'pypi', 'total',
+
+    def compute_total(self):
+        self.total = (self.gem + self.maven + self.nuget + self.npm + self.pypi)
+
+
+def compute_median(datapoints):
+    """
+    Return a median score Aggregate
+    """
+    median = Aggregate('score median')
+
+    # make each package type a list attribute that we will use to compute the median
+    for a in Aggregate.ptypes:
+        setattr(median, a, list())
+
+    # accumulate values
+    for datapoint in datapoints:
+        di = getattr(median, datapoint.type)
+        di.append(datapoint.score)
+        di = getattr(median, 'total')
+        di.append(datapoint.score)
+
+    import statistics
+    for a in Aggregate.ptypes:
+        dl = getattr(median, a)
+        med = dl and statistics.median(dl) or 0
+        setattr(median, a, med)
+
+    return median
+
+
+def compute_aggregates(results):
+    """
+    Return a mapping of aggregates as {name: data} where the data for an
+    aggregate is a table which is a list of Aggregate objects.
+    {'Table 1. Totals by type':
+        [
+            Aggregate(label='score',gem=12, maven=23,...),
+            Aggregate(label='discovered',gem=12, maven=23,...),
+            Aggregate(label='score',gem=12, maven=23,...),
+        ],
+     'Table 2. XXXX  ptype':
+        {
+        ...
+        }
+    }
+    """
+    datapoints = []
+    for package in results:
+        datapoint = Datapoint(**{k:v for k, v in package.items() if k in dp_fields})
+        datapoints.append(datapoint)
+
+    aggregate_tables = OrderedDict()
+
+    ############################################################################
+    aggregate_name = 'Table.1 Tallies for each scoring element by package type (averages for score and discovered)'
+    tallies = OrderedDict()
+    package_counts = Counter()
+    def update_count(_label, _tallies, _datapoint):
+        _tally = _tallies.get(_label)
+        if _tally is None:
+            _tally = Aggregate(label=_label)
+            _tallies[_label] = _tally
+        _value = getattr(_tally, _datapoint.type) + getattr(_datapoint, _label, 0)
+        setattr(_tally, _datapoint.type, _value)
+
+    for datapoint in datapoints:
+        package_counts[datapoint.type] += 1
+        for label in ('score', 'declared', 'discovered', 'consistency', 'spdx', 'full_text',):
+            update_count(label, tallies, datapoint)
+
+    # we prefix with median and suffix with counts
+    median = compute_median(datapoints)
+    aggregate_tables[aggregate_name] = [median] + list(tallies.values())
+
+    # append package counts
+    package_counts['total'] = sum(package_counts.values())
+    package_counts_aggregate = Aggregate('package_counts', **package_counts)
+    aggregate_tables[aggregate_name].append(package_counts_aggregate)
+
+    # update totals
+    for tally in  tallies.values():
+        tally.compute_total()
+        if tally.label == 'score':
+            # compute a ratio between 0 and 100
+            for type in Aggregate.ptypes:
+                value = getattr(tally, type)
+                setattr(tally, type, round(value / package_counts[type], 1))
+            tally.label = 'score average'
+
+        if tally.label == 'discovered':
+            # compute a ratio between 0 and 100
+            for type in Aggregate.ptypes:
+                value = getattr(tally, type)
+                setattr(tally, type, round((value / package_counts[type]) * 100, 1))
+            tally.label = 'discovered average'
+
+
+    ############################################################################
+    aggregate_name = 'Table 2. Scoring elements percentage by package type'
+    percentages = OrderedDict()
+    for datapoint in datapoints:
+        for label in ('score', 'declared', 'discovered', 'consistency', 'spdx', 'full_text',):
+            update_count(label, percentages, datapoint)
+
+    aggregate_tables[aggregate_name] = percentages.values()
+
+    # compute total and averages for discrete values
+    for percent in percentages.values():
+        percent.compute_total()
+        if percent.label not in ('score', 'discovered',):
+            # compute a ratio between 0 and 100
+            for type in Aggregate.ptypes:
+                value = getattr(percent, type)
+                setattr(percent, type, round((value / package_counts[type]) * 100, 1))
+            percent.label = 'percentage with {}'.format(percent.label)
+
+        if percent.label == 'score':
+            # compute a ratio between 0 and 100
+            for type in Aggregate.ptypes:
+                value = getattr(percent, type)
+                setattr(percent, type, round(value / package_counts[type], 1))
+            percent.label = 'score average'
+
+        if percent.label == 'discovered':
+            # compute a ratio between 0 and 100
+            for type in Aggregate.ptypes:
+                value = getattr(percent, type)
+                setattr(percent, type, round((value / package_counts[type]) * 100, 1))
+            percent.label = 'discovered average'
+
+
+    ############################################################################
+    aggregate_name = 'Table 3. Number of packages by license score brackets and by type'
+    brackets = OrderedDict([
+        (bk, Aggregate(label=blabel)) for bk, blabel in brackets_labels.items()
+    ])
+
+    def update_bracket(_bracket_key, _brackets, _datapoint_type):
+        _bracket = _brackets.get(_bracket_key)
+        _value = getattr(_bracket, _datapoint_type) + 1
+        setattr(_bracket, _datapoint_type, _value)
+
+    for datapoint in datapoints:
+        update_bracket(datapoint.score_bracket, brackets, datapoint.type)
+
+    # update totals
+    for bracket in brackets.values():
+        bracket.compute_total()
+
+    aggregate_tables[aggregate_name] = brackets.values()
+    aggregate_tables[aggregate_name].append(package_counts_aggregate)
+
+
+    ############################################################################
+    aggregate_name = 'Table 4. Number of packages by percentage of discovered license brackets and by type'
+    brackets = OrderedDict([
+        (bk, Aggregate(label=blabel)) for bk, blabel in brackets_labels.items()
+    ])
+
+    for datapoint in datapoints:
+        update_bracket(datapoint.discovered_bracket, brackets, datapoint.type)
+
+    # update totals
+    for bracket in  brackets.values():
+        bracket.compute_total()
+
+    aggregate_tables[aggregate_name] = brackets.values()
+    aggregate_tables[aggregate_name].append(package_counts_aggregate)
+
+
+    ############################################################################
+    # aggregate_name= 'Table 4. Number of packages by type above a license score threshold'
+
+
+    return aggregate_tables
 
 
 def fetch_package(url, download_dir):
@@ -272,22 +526,20 @@ def create_dir(name, base_dir=current_dir):
 
 if __name__ == '__main__':
 
-    fetch = True
-    args = sys.argv[1:]
-    if args:
-        arg0 = args[0]
-        if arg0 == '--no-fetch':
-            do_fetch = False
-            do_extract = False
+    do_fetch = False
+    do_extract = False
+    do_scan = False
+    do_rescore = False
 
+    # directory where the downloads are fetched, extracted and scanned
     base_dir = os.environ.get('CLEARDATA', current_dir)
 
     compute_license_score(
         base_dir=base_dir,
         do_fetch=do_fetch,
         do_extract=do_extract,
-        do_scan=False,
-        do_rescore=True,
+        do_scan=do_scan,
+        do_rescore=do_rescore,
         types=set([
             'gem',
             'maven',
